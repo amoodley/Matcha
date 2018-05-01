8c1b167542fa54bf86a6fa6a13d7e30b29bd31ee
Ashleigh Moodley <amoodley@student.wethinkcode.co.za>
2018-05-01 18:36:37 +0200

    Registration working with DBand Hash

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
 const express = require('express');
+const mysql = require('mysql');
 const bodyParser = require('body-parser');
 const path = require('path');
+const bcrypt = require('bcrypt');
 
 const hostname = '127.0.0.1';
 const port = 3000;
-const app = express();
 
-// var logger = function(req, res, next){
-// 	console.log('logging...');
-// }
-// app.use(logger);
+// Create DB Connection
+const db = mysql.createConnection({
+	port	 : '3307',
+	host     : 'localhost',
+    user     : 'root',
+    password : 'root',
+    database : 'matcha'
+});
+
+// Connect
+db.connect((err) => {
+    if(err){
+        console.log('Error: ', err);
+    }
+    console.log('MySql connected...')
+})
+
+// Create App
+const app = express();
 
 // View Engine
 app.set('view engine', 'ejs');
@@ -25,76 +41,115 @@ app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); /
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
 
+// Create DB
+// app.get('/createdb', (req, res) => {
+// 	let sql = 'CREATE DATABASE nodemysql';
+// 	db.query(sql, (err, result) => {
+// 		if(err) {
+// 			console.log('Error: ', err);
+// 		}
+// 		console.log(result);
+// 		res.send('database created...');
+// 	})
+// })
+
 // GET: Home
 app.get('/', (req, res) => {
 
 	res.render('index', {
-		title: 'Home'
+		title: 'Home',
 	});
 });
 
 // GET: Register
-app.get('/users/register', (req, res) => {
+app.get('/register', (req, res) => {
 
 	res.render('register', {
-		title: 'Register'
+		title: 'Register',
+		username: '',
+		email: '',
+		message: {
+			Username: '',
+			Password: '',
+			Email: ''
+		}
 	});
 });
 
 // POST: Register
-app.post('/users/register', (req, res) => {
+app.post('/register', (req, res) => {
 	
-	var message = '';
+	var message = {
+		Username: '',
+		Password: '',
+		Email: ''
+	}
 	var username = req.body.username;
 	var password = req.body.password;
+	var email = req.body.email;
 
-	if (req.body.username != null) {
-
-		if (username.length > 3) {
-
-			if (username.length < 31) {
-
-				if (req.body.email != null)	{
-
-					if (password != null) {
-
-						if (password.length > 6) {
-
-							if (password < 31) {
-
-								var newUser = {
-
-									username: req.body.username,
-									email: req.body.email,
-									password: req.body.password
-								}
-							}
-							message = 'Password cannot be longer than 30'
-						}
-						message = 'Password must be longer than 6'
-					}
-					message = 'Email cannot be null';
-				}
-				message = 'Username cannot be longer than 30'
-			}
-			message = 'Username must be greater than 3 characters';
+	if (username != '') {
+		if (username.length < 3) {
+			message.Username = 'Username cannot be shorter than 3';
 		}
-		message = 'Username cannot be null';
+		else if (username.length > 30) {
+			message.Username = 'Username cannot be longer than 30'
+		}
+	}
+	else {
+		message.Username = 'Username cannot be empty';
 	}
 	
-	if (message != null) {
+	if (req.body.email == '')	{
+		message.Email = 'Email cannot be empty';
+	}
+
+	if (password != '') {
+		if (password.length < 6) {
+		message.Password = 'Password cannot be shorter than 6'
+		}
+		else if (password.length > 30) {
+			message.Password = 'Password cannot be longer than 30'
+		}
+	}
+	else {
+		message.Password = 'Password cannot be empty'
+	}
+	
+	if ((message.Username == '') && (message.Password == '') && (message.Email == '')) {
+		var newUser = {
+			id: null,
+			username: req.body.username,
+			password_hash: '',
+			email: req.body.email,
+			activated: 0,
+			state: 1
+		}
+		let hash = bcrypt.hashSync(password, 10);
+		newUser.password_hash = hash;
+		let sql = 'INSERT INTO `users` (id, username, password_hash, email, activated, state) VALUES(?)';
+		var values = [newUser.id, newUser.username, newUser.password_hash, newUser.email, newUser.activated, newUser.state]
+		let query = db.query(sql, [values], (err, result) => {
+			if(err) {
+				console.log('Error: ', err);
+			}
+			console.log(result);
+			res.send('User inserted...');
+		})
+	}
+	else {
 		res.render('register', {
 			title: 'Register',
+			username: username,
+			email: email,
 			message: message
 		});
 	}
-	else {
-		console.log(newUser);
-	}
-
-	
 });
 
+
+
+// Start Server
 app.listen(3000, function(){
   console.log(`Server running at http://${hostname}:${port}/`);
 });
--- database/Connection.js
+++ database/Connection.js
@@ -1,19 +1,36 @@
-var mysql = require('mysql');
-var connection = mysql.createConnection({
+var express   =    require("express");
+var mysql     =    require('mysql');
+var app       =    express();
+
+var pool      =    mysql.createPool({
+    connectionLimit : 100, //important
     host     : 'localhost',
     user     : 'root',
     password : 'root',
-    database : 'matcha'
+    database : 'matcha',
+    debug    :  false
 });
 
-connection.connect();
+function handle_database(req,res) {
+    
+    pool.getConnection(function(err,connection){
+        if (err) {
+          res.json({"code" : 100, "status" : "Error in connection database"});
+          return;
+        }
 
-connection.query('SELECT * FROM users', 
-function(err, rows, fields) {
-    if(!err)
-        console.log('The solution is: ', rows);
-    else
-        console.log('Error while performing Query');
-});
+        console.log('connected as id ' + connection.threadId);
+        
+        connection.query(sql, function(err,rows){
+            connection.release();
+            if(!err) {
+                res.json(rows);
+            }
+        });
 
-connection.end();
+        connection.on('error', function(err) {
+              res.json({"code" : 100, "status" : "Error in connection database"});
+              return;
+        });
+  });
+}
