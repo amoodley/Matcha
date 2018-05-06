9e1f947beb9d857dc5537dc3b25a704071082df5
Ashleigh Moodley <amoodley@student.wethinkcode.co.za>
2018-05-06 16:02:08 +0200

    Setup login redirect for homeView

--- routes/home.js
+++ routes/home.js
@@ -1,11 +1,21 @@
-var express = require('express')
-var router = express.Router()
+var express = require('express');
+var router = express.Router();
+const cookieParser = require('cookie-parser');
+
+const users = require('../models/users.js');
+
 
 // define the home page route
 router.get('/', (req, res) => {
-	res.render('home/index', {
-		title: 'Home',
-	});
+
+	if (users.isLoggedIn(req)) {
+		var userId = users.isLoggedIn(req);
+		res.render('home/index', {
+			title: 'Home',
+		});
+	} else {
+		res.redirect('/account/login');
+	}
 });
 
 module.exports = router;
