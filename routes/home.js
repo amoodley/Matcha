f356bf353d12c08459c7e5d3f2a5ff82f26d81c0
Ashleigh Moodley <amoodley@student.wethinkcode.co.za>
2018-05-10 00:51:03 +0200

    AddedsetupProfile route

--- routes/home.js
+++ routes/home.js
@@ -7,15 +7,26 @@ const users = require('../models/users.js');
 
 // define the home page route
 router.get('/', (req, res) => {
-
 	if (users.isLoggedIn(req)) {
 		var userId = users.isLoggedIn(req);
-		res.render('home/index', {
-			title: 'Home',
-		});
+		var result = users.getUserById(userId);
+		console.log(result.state);
+		if (result.state == 1) {
+			res.redirect('/setupProfile');
+		} else {
+			res.render('home/index', {
+				title: 'Home',
+			});
+		}
 	} else {
 		res.redirect('/account/login');
 	}
+});
+
+router.get('/setupProfile', (req, res) => {
+	res.render('home/setupProfile', {
+		title: 'Setup profile'
+	});
 });
 
 module.exports = router;
