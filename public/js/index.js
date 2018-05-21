18d27847736d4eda703cae67871f82ad07c3d469
Ashleigh Moodley <amoodley@student.wethinkcode.co.za>
2018-05-21 22:19:06 +0200

    Added #likesTab Ajax

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,6 @@
 function search() {
+    $('#searchButton').html('Searching');
+    $('#searchResults').empty();
     var search = {
         preference: document.getElementById('searchPreference').value,
         fromAge: document.getElementById('searchFromAge').value,
@@ -15,26 +17,26 @@ function search() {
             'distance': search.distance
         },
         success: (result) => {
-            if (result){
-                $('#searchResults').empty();
+            if (result) {
+                $('#searchButton').html('Search');
                 var searchResult = document.getElementById('searchResult');
                 result.forEach(element => {
                     var birthday = element.birthday.substring(0, 10);
                     $('#searchResults').append(`
                         <div class="searchResultCard">
                             <div class="searchResultCardImage">
-                                <img src="`+ element.profileimg +`" class="img-responsive img-circle">
+                                <img src="`+ element.profileimg + `" class="img-responsive img-circle">
                             </div>
                             <div class="searchResultCardInfo text-center">
-                                <a href="/profile/`+ element.username +`">
-                                    <h3>`+ element.username +`</h3>
+                                <a href="/profile/`+ element.username + `">
+                                    <h3>`+ element.username + `</h3>
                                 </a>
-                                <p class="myDate">`+ element.age +`, `+ element.city +`</p>
+                                <p class="myDate">`+ element.age + `, ` + element.city + `</p>
                             </div>
                             <div class="searchResultCardTagLine">
-                                <p>`+ element.bio +`</p>
+                                <p>`+ element.bio + `</p>
                                 <p class="profileCardLabel">Interests</p>
-                                <p>`+ element.interests +`</p>
+                                <p>`+ element.interests + `</p>
                             </div>
                         </div>`
                     );
@@ -58,6 +60,43 @@ $("#viewsTab").click(function () {
                 result.forEach(element => {
                     var birthday = element.birthday.substring(0, 10);
                     $('#viewResults').append(`
+                        <div class="searchResultCard">
+                            <div class="searchResultCardImage">
+                                <img src="`+ element.profileimg + `" class="img-responsive img-circle">
+                            </div>
+                            <div class="searchResultCardInfo text-center">
+                                <a href="/profile/`+ element.username + `">
+                                    <h3>`+ element.username + `</h3>
+                                </a>
+                                <p class="myDate">`+ element.age + `, ` + element.city + `</p>
+                            </div>
+                            <div class="searchResultCardTagLine">
+                                <p>`+ element.bio + `</p>
+                                <p class="profileCardLabel">Interests</p>
+                                <p>`+ element.interests + `</p>
+                            </div>
+                        </div>`
+                    );
+                });
+            }
+        },
+        error: (err) => {
+            console.log(err);
+        }
+    });
+});
+
+$("#likesTab").click(function () {
+    $.ajax({
+        url: "/likes",
+        type: "GET",
+        success: (result) => {
+            if (result) {
+                $('#likesResults').empty();
+                var likesResults = document.getElementById('likesResults');
+                result.forEach(element => {
+                    var birthday = element.birthday.substring(0, 10);
+                    $('#likesResults').append(`
                         <div class="searchResultCard">
                             <div class="searchResultCardImage">
                                 <img src="`+ element.profileimg + `" class="img-responsive img-circle">
