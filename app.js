c15af9e153a5ee0f34ef6b7a3326109ba46aafa3
Ashleigh Moodley <amoodley@student.wethinkcode.co.za>
2018-05-24 11:50:37 +0200

    Added Isotope path

--- app.js
+++ app.js
@@ -35,6 +35,8 @@ app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); /
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
 app.use('/js', express.static(__dirname + '/node_modules/masonry-layout/dist')); // redirect Masonry Layout
+app.use('/js', express.static(__dirname + '/node_modules/isotope-layout/dist')); // redirect Isotope Layout
+
 
 
 // Set Routes
