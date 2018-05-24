47acfcba026580c8f7e55f2f06ae856cc7315bee
Ashleigh Moodley <amoodley@student.wethinkcode.co.za>
2018-05-24 09:24:49 +0200

    Added Masonry JS path

--- app.js
+++ app.js
@@ -34,6 +34,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
+app.use('/js', express.static(__dirname + '/node_modules/masonry-layout/dist')); // redirect Masonry Layout
+
 
 // Set Routes
 app.use('/', home);
