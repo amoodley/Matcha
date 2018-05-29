c33d7e4781af5fc3e3996812c3d3dd150dbfdc2d
Ashleigh Moodley <amoodley@student.wethinkcode.co.za>
2018-05-29 19:10:25 +0200

    Added SaveMessageToDB

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ const path = require('path');
 const hostname = '127.0.0.1';
 const port = 3000;
 
+// Load Models
+const db = require('./database/db');
+const users = require('./models/users.js');
+const profiles = require('./models/profiles.js');
+
 // Load Routes
 const home = require('./routes/home');
 const account = require('./routes/account');
@@ -50,11 +55,6 @@ app.use('/account', account);
 app.use('/profile', profile);
 
 
-// // Start Server
-// server.listen(3000, function(){
-//   console.log(`Server running at http://${hostname}:${port}/`);
-// });
-
 io.sockets.on('connection', function(socket){
   connections.push(socket);
   console.log('Connected: %s sockets connected', connections.length);
@@ -67,8 +67,28 @@ io.sockets.on('connection', function(socket){
 
   // Send Message
   socket.on('send message', function(data){
-    console.log(data);
     io.sockets.emit('new message', {msg: data.msg, username: data.username, recipient: data.recipient});
+    var chatResult = db.query('SELECT chat_id FROM chats WHERE user_id=\'' + data.username + '\' AND recipient_id=\'' + data.recipient + '\'').data.rows;
+    if (chatResult[0] == undefined) {
+      chatResult = db.query('SELECT chat_id FROM chats WHERE user_id=\'' + data.recipient + '\' AND recipient_id=\'' + data.username + '\'').data.rows;
+      if (chatResult[0] == undefined) {
+        var sql = 'INSERT INTO `chats` (id, chat_id, user_id, recipient_id, message) VALUES(?)';
+        var values = [null, null, data.username, data.recipient, data.msg];
+        var chatId = db.query(sql, [values]).data.rows.insertId;
+        var result = db.query('UPDATE `chats` SET chat_id=\'' + chatId + '\' WHERE id=\'' + chatId + '\'');
+      } else {
+        var sql = 'INSERT INTO `chats` (id, chat_id, user_id, recipient_id, message) VALUES(?)';
+        var values = [null, null, data.username, data.recipient, data.msg];
+        var id = db.query(sql, [values]).data.rows.insertId;
+        var chatId = chatResult[0].chat_id;
+        var result = db.query('UPDATE `chats` SET chat_id=\'' + chatId + '\' WHERE id=\'' + id + '\'');
+      }
+    } else {
+        var sql = 'INSERT INTO `chats` (id, chat_id, user_id, recipient_id, message) VALUES(?)';
+        var values = [null, null, data.username, data.recipient, data.msg];
+        var id = db.query(sql, [values]).data.rows.insertId;
+        var chatId = chatResult[0].chat_id;
+        var result = db.query('UPDATE `chats` SET chat_id=\'' + chatId + '\' WHERE id=\'' + id + '\'');
+    }
   });
-  
 });
